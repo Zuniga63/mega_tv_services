a160398a8cb21dfef93e32adb1212fe58aea0ace
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-05-06 13:40:56 -0500

    feat: add initial config of swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // SWAGGER CONFIG
+  const config = new DocumentBuilder()
+    .setTitle('Mega Tv Service')
+    .setDescription(
+      'MEGA Services is a new platform to contract television services in a building. The main objective is to help users contract a cable TV service for their home.',
+    )
+    .setVersion('1.0')
+    .addTag('Buildings')
+    .addTag('TvPlan')
+    .addTag('Customers')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
