a706cec05b3f7639922e6f53e1d432b87c5a26a3
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-05-06 21:38:40 -0500

    docs: add the subscriptions to the BuildingDto

--- server/src/modules/buildings/dto/building.dto.ts
+++ server/src/modules/buildings/dto/building.dto.ts
@@ -20,4 +20,43 @@ export class BuildingDto {
 
   @ApiProperty({ example: '10038' })
   zipCode: string;
+
+  @ApiProperty({
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'number',
+          example: 1,
+        },
+        subscriptionDate: {
+          type: 'string',
+          format: 'date-time',
+        },
+        appNumber: {
+          type: 'string',
+          example: '112',
+        },
+        tvPlan: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'number',
+              example: 1,
+            },
+            name: {
+              type: 'string',
+              example: 'Basic',
+            },
+            price: {
+              type: 'number',
+              example: 50,
+            },
+          },
+        },
+      },
+    },
+  })
+  subscriptions: any[];
 }
